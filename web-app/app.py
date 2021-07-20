e29b33df5fb3154527a8b9771a421b78c1a64fde
tmsteph <tmsteph1290@gmail.com>
2021-07-19 17:47:03 -0700

    added app.py. Not sure why it was missing

---
+++ web-app/app.py
@@ -0,0 +1,22 @@
+# pip install flask-bootstrap
+
+from flask import Flask
+from flask import render_template  
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/test')
+def hello():
+	return 'Hello World!!!!'
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
